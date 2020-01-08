992f33d2acea5b232a1045ffac0855b9f034a864
Rafael R. Nunes <rafael@rarotecnologia.com.br>
2020-01-08 12:41:44 -0300

    Aula de Exemplo 03

---
+++ Variaveis/exemplo-03.php
@@ -0,0 +1,26 @@
+<?php
+
+$nome = "HCode";
+$site = 'www.hcode.com.br';
+
+$ano = 1990;
+$salario = 5500.99;
+$bloqueado = false;
+
+//////////////////////////////////////////////////////
+
+$frutas = array("abacaxi", "laranja", "manga")
+
+echo $frutas[2];
+
+$nascimento = new DateTime();
+//var_dump($nascimento);
+
+//////////////////////////////////////////////////////
+
+$arquivo = fopen("exemplo-03.php");
+//var_dump($arquivo);
+
+$nulo = NULL;
+
+?>
